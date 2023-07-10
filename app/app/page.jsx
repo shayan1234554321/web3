2c97ba18fe16b1d31b86cb191663a44f1ca72b16
shayan1234554321 <kshayan091@gmail.com>
2023-07-11 02:41:48 +0500

    frontend welcome screen

---
+++ app/app/page.jsx
@@ -0,0 +1,21 @@
+
+"use client"
+import Navbar from "@/components/navbar"
+import Welcome from "@/components/welcome"
+import Footer from "@/components/footer"
+import Services from "@/components/services"
+import Transactions from "@/components/transactions"
+
+export default function Home() {
+  return (
+    <div className="min-h-screen" >
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
+    </div>
+  )
+}
